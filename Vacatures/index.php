866018ee29ee6d2272c2c0c20657d8f77094644d
6042272 <144230773+6042272@users.noreply.github.com>
2024-04-12 09:18:59 +0200

    Vacatures pagina aangemaakt en toegeovegd aan header

---
+++ Vacatures/index.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="nl">
+<head>
+    <meta charset="utf8">
+    <meta name="viewport" content="width-device-width, initial-scale=1">
+    <meta name="description" content="Vacatures pagina van restaurant Hot dasi Handi">
+    <meta name="author" content="L. Bouwmeester">
+    <meta name="keywords" content="">
+    <title>Hot Dasi Handi - Vacatures</title>
+    <link rel="stylesheet" type="text/css" href="../css/style.css">
+</head>
+
+<body>
+    <?php include "../php_shared/header.php"; ?>
+
+    <main>
+        
+    </main>
+
+    <?php include "../php_shared/footer.php"; ?>
+</body>
+</html>
