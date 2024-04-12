4f4f5cde6307654a03375c3c98c93048a2c30d67
6042272 <144230773+6042272@users.noreply.github.com>
2024-04-12 09:27:16 +0200

    Vacatures restaurant image, header foto met tekst erop, css hiervoor

--- Vacatures/index.php
+++ Vacatures/index.php
@@ -15,6 +15,17 @@
 
     <main>
         
+        <section class="vacature_image">
+            <img src="img/restaurant.jpg" alt="Gallerij afbeelding">
+
+            <section class="text_overlay">
+                <h1>Werken bij ons?</h1>
+                <p>Bij ons werken is een unieke ervaring. We zijn altijd op zoek naar getalenteerde en gepassioneerde individuen om ons team te versterken. Als je een passie hebt voor gastvrijheid, lekker eten en teamwork, dan ben je bij ons aan het juiste adres!</p>
+                <p>We bieden een stimulerende werkomgeving, mogelijkheden voor professionele ontwikkeling en groei, en een hecht team dat elkaar ondersteunt. Of je nu ervaring hebt in de horeca of net begint aan je carrière, we verwelkomen iedereen die gemotiveerd is om te leren en te groeien.</p>
+                <p>Als je geïnteresseerd bent om deel uit te maken van ons team, stuur dan je CV en een korte motivatiebrief naar ons via het contactformulier op onze website. We kijken ernaar uit om van je te horen!</p>
+            </section>
+        </section>
+
     </main>
 
     <?php include "../php_shared/footer.php"; ?>
