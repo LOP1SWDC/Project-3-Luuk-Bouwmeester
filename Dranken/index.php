009e820b9e9aae9694d49ae611544e932229a4a9
6042272 <144230773+6042272@users.noreply.github.com>
2024-04-12 08:55:55 +0200

    Main dranken index met php en css uitgewerkt

--- Dranken/index.php
+++ Dranken/index.php
@@ -16,6 +16,58 @@
 
     <main>
 
+    <section class="dranken_main">
+
+            <section class="dranken_text">
+                <h1>Welkom bij onze dranken!</h1>
+                <p>Hier vindt je al onze dranken.</p>
+            </section>
+
+            <section class="dranken_containers">
+                <?php
+                    $gerechten = [
+                        [
+                            "naam" => "7 up",
+                            "img" => "img/7up.jpg",
+                            "prijs" => "$2,75"
+                        ],
+                        [
+                            "naam" => "Cola",
+                            "img" => "img/cola.jpg",
+                            "prijs" => "$2,50"
+                        ],
+                        [
+                            "naam" => "Dr pepper",
+                            "img" => "img/dr_pepper.jpg",
+                            "prijs" => "$2,00"
+                        ],
+                        [
+                            "naam" => "Pepsi",
+                            "img" => "img/pepsi.jpg",
+                            "prijs" => "$2,50"
+                        ],
+                        [
+                            "naam" => "Ice tea",
+                            "img" => "img/ice_tea.jpg",
+                            "prijs" => "$2,50"
+                        ],
+                        [
+                            "naam" => "Fanta",
+                            "img" => "img/fanta.jpg",
+                            "prijs" => "$2,25"
+                        ],
+                    ];
+
+                    foreach ($gerechten as $gerecht) {
+                        echo '<section class="drank_single">';
+                        echo '<img src=' . $gerecht['img'] . '>';
+                        echo '<h1>' . $gerecht['naam'] . '</h1>';
+                        echo '<p>Prijs: ' . $gerecht['prijs'] . '</p>';
+                        echo '</section>';
+                    }
+                ?>
+        </section>
+
     </main>
 
     <?php include "../php_shared/footer.php"; ?>
