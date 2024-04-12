6013a45d58edb90c8d3976f925c6b3de0f77ae25
6042272 <144230773+6042272@users.noreply.github.com>
2024-04-12 09:11:15 +0200

    Arrangements index en css begin

--- Arrangements/index.php
+++ Arrangements/index.php
@@ -15,6 +15,69 @@
     <?php include "../php_shared/header.php"; ?>
 
     <main>
+        
+        <section class="arrangements_text">
+            <h1>Op zoek naar een arrangement?</h1>
+            <p>Deze vindt je hier!</p>
+        </section>
+
+        <section class="arrangement_single">
+            <h1>Tandoori Feast Combo</h1>
+            <br>
+            <p>Tandoori Kip - $15,50</p>
+            <br>
+            <p>Naan Brood - $4,50</p>
+            <br>
+            <p>Mango Lassi - $4,99</p>
+        </section>
+
+        <section class="arrangement_single">
+            <h1>Indian Delight Combo</h1>
+            <br>
+            <p>Butter Chicken - $14,99</p>
+            <br>
+            <p>Palak Paneer - $12,99</p>
+            <br>
+            <p>Samosa - $3,99</p>
+        </section>
+
+        <section class="arrangement_single">
+            <h1>Seafood Special Combo</h1>
+            <br>
+            <p>Prawn Biryani - $18,50</p>
+            <br>
+            <p>Ice Tea - $14,99</p>
+        </section>
+
+        <section class="arrangement_single">
+            <h1>Vegetarian Delight Combo</h1>
+            <br>
+            <p>Palak Paneer - $12,99</p>
+            <br>
+            <p>Naan Brood - $4,50</p>
+            <br>
+            <p>Mango Lassi - $4,99</p>
+        </section>
+
+        <section class="arrangement_single">
+            <h1>Spicy Trio Combo</h1>
+            <br>
+            <p>Tandoori Kip - $15,50</p>
+            <br>
+            <p>Samosa - $3,99</p>
+            <br>
+            <p>Cola - $14,99</p>
+        </section>
+
+        <section class="arrangement_single">
+            <h1>Family Feast Combo</h1>
+            <br>
+            <p>Butter Chicken - $14,99</p>
+            <br>
+            <p>Prawn Biryani - $18,50</p>
+            <br>
+            <p>7 Up - $15,50</p>
+        </section>
 
     </main>
 
